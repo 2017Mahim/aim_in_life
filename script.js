a7678039b3df873b9242d84fb380243b21c34ddd
2017Mahim <106513058+2017Mahim@users.noreply.github.com>
2023-03-30 01:19:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const name = prompt("What's your name?");
-const passion = prompt("What did you wanted to be?");
+const passion = prompt("What do you want to be?");
 const age = prompt("How much is your age?");
 const assert = document.getElementById("assert");
-// const iq = 200;
-const assertive = `${name}, shono, ${age} bosor boyoshe ${passion} hote chaw. ${passion} boilla tmr hogar vitre voira dimu`;
+const assertive = `${name}, shono, ${age} bosor boyoshe "${passion}" hote chaw. "${passion}" boilla tmr hogar vitre voira dimu`;
 const textNode = document.createTextNode(assertive);
 assert.appendChild(textNode);
